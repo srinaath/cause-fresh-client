fb3c2c6d143d2db1d0fed3358105d2f657e96b4b
Srinaath Ravichandran <sravichandran@deloitte.com>
2017-12-13 16:53:19 -0700

    Added scss support
    Signed-off-by: Srinaath Ravichandran <sravichandran@deloitte.com>

---
+++ src/components/mega-nav/mega-nav.js
@@ -0,0 +1,33 @@
+/**
+
+  @name MegaNav
+  @description Header navigation
+
+  @memberof Components
+  @see {@link MegaNav}
+  @author Srinaath Ravichandran <sravichandran@deloitte.com>
+
+  * @example
+  * // client/index.js
+  * render(
+  *   <MegaNav />
+  * )
+*/
+
+// STYLES
+
+// THIRD-PARTY
+import React, { Component } from 'react';
+import './mega-nav.css';
+
+class MegaNav extends Component {
+  render() {
+    return (
+      <header className="mega-nav">
+        <h1> Hello </h1>
+      </header>
+    );
+  }
+}
+
+export default MegaNav;
