d168a06670c9a6202178ad7f3c237e60f6e73870
Patton, Daniel (US - Denver) <dpatton@deloitte.com>
2018-02-10 15:51:15 -0700

    styling causes page

--- src/containers/causes/causes-container.js
+++ src/containers/causes/causes-container.js
@@ -1,4 +1,5 @@
 // THIRD-PARTY
+import {Link, Route} from 'react-router-dom';
 import React, { Component }   from 'react';
 import { connect }            from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -27,6 +28,22 @@ class CausesContainer extends Component {
         <LeftNav />
         <div className="col-xs-9 mainContentWrapper">
           <h1><i className="fa fa-globe" />&nbsp; Causes</h1>
+
+          <div className="row">
+             <div className="col-xs-4">
+               <ul className="causeBoxes">
+                 <li className="causeBox">
+                   <h3>American Red Cross</h3>
+                   <p>Johnson County Relief Fund</p>
+                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id urna vel dui euismod facilisis quis quis risus. 
+                   Proin et suscipit urna. Pellentesque at commodo massa, a suscipit orci. Praesent lobortis posuere ante at faucibus. 
+                   Etiam lobortis, est volutpat commodo facilisis, diam lectus egestas magna, ut ornare arcu odio eget dui. <br />
+                   </p>
+                   <p><Link to={'/donation'} className="donate">Donate</Link></p>
+                 </li>
+               </ul>
+             </div>
+           </div>
         </div>
       </div>
     </div>
