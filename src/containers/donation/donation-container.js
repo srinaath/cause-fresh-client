b67f588ce76af536f984bee4bb3df25c868034ab
Srinaath Ravichandran <sravichandran@deloitte.com>
2018-02-10 15:30:54 -0700

    Donation container update
    Signed-off-by: Srinaath Ravichandran <sravichandran@deloitte.com>

--- src/containers/donation/donation-container.js
+++ src/containers/donation/donation-container.js
@@ -2,8 +2,9 @@
 import React, { Component }   from 'react';
 import { connect }            from 'react-redux';
 import { bindActionCreators } from 'redux';
+import axios from 'axios';
 
-// COMPONENTS 
+// COMPONENTS
 import LeftNav from '../../components/left-nav/left-nav';
 
 //REDUX
@@ -16,9 +17,52 @@ class DonationContainer extends Component {
 
   constructor(props) {
     super(props);
+    this.getDonationData = this.getDonationData.bind(this);
+    this.updateDonations = this.updateDonations.bind(this);
+    this.onDonationChange = this.onDonationChange.bind(this);
+    this.state = {
+      donationsData: {},
+      donationAmt: ''
+    };
   }
 
-  componentWillReceiveProps(nextProps) {}
+  componentWillReceiveProps(nextProps) {
+
+  }
+
+  onDonationChange(evt) {
+    this.setState({
+      donationAmt: Number(evt.target.value)
+    });
+  }
+
+  getDonationData() {
+    return axios.get(`http://localhost:4000/api/getDonationScreenData`)
+    .then((response) => {
+      return response.data
+    })
+    .catch((error) => {
+      return null;
+    });
+  }
+
+
+  updateDonations(evt) {
+    evt.stopPropogation();
+    const selectedOrg = document.getElementById("org").selectedIndex;
+    const selectedCause = document.getElementById("cause").selectedIndex;
+    const selectedSubCause = document.getElementById("subcause").selectedIndex;
+    const donationAmt = document.getElementById("donationTxt").value;
+  }
+
+
+  componentDidMount() {
+    this.getDonationData().then((data) => {
+      this.setState({
+        donationsData : data
+      });
+    });
+  }
 
   render() {
     return (
@@ -31,28 +75,32 @@ class DonationContainer extends Component {
           </h1>
           <form>
             <div className="select-style">
-              <select>
-                <option value="1">American Red Cross</option>
+              <select id="org">
+                {this.state.donationsData.orgs && this.state.donationsData.orgs.map((org, index) => {
+                  return <option>{org.orgName}</option>;
+                })}
               </select>
             </div>
             <br />
             <div className="select-style">
-              <select>
-                <option value="1">Katrina Relief</option>
+              <select id="cause">
+                {this.state.donationsData.causes && this.state.donationsData.causes.map((causes, index) => {
+                  return <option>{causes.causeName}</option>;
+                })}
               </select>
             </div>
             <br />
             <div className="select-style">
-              <select>
-                <option value="1">General</option>
-                <option value="2">Food/Water</option>
-                <option value="3">Supplies</option>
+              <select id="subcause">
+                {this.state.donationsData.subCauses && this.state.donationsData.subCauses.map((subCauses, index) => {
+                  return <option>{subCauses.causeDetailName}</option>;
+                })}
               </select>
             </div>
             <br />
-            <input type="text" placeholder="Donation Amount" />
+            <input type="text" id ="donationTxt" placeholder="Donation Amount" onChange = {this.onDonationChange}/>
             <br />
-            <input type="submit" value="Donate" />
+            <input type="submit" value="Donate" onClick={this.updateDonations} placeholder="Donate in MetaCoins"/>
           </form>
         </div>
       </div>
